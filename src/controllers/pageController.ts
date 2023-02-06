dc46eea84064d98428015b5d42a7edc95fccb073
SerjeiMikailov <felipemaders8@gmail.com>
2023-02-06 14:20:56 -0300

    g

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,61 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response)=>{
-    res.render('pages/page.mustache')
+    res.render('pages/page.mustache', {
+        menu: {
+            all: true,
+            dogs: false,
+            cats: false,
+            fishes: false
+        },
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    })
 }
 
 export const dogs = (req: Request, res: Response)=>{
-    res.send('dogs')
+    res.render('pages/page.mustache', {
+        menu: {
+            all: false,
+            dogs: true,
+            cats: false,
+            fishes: false
+        },
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    })
 }
 
 export const cats = (req: Request, res: Response)=>{
-    res.send('cats')
+    res.render('pages/page.mustache', {
+        menu: {
+            all: false,
+            dogs: false,
+            cats: true,
+            fishes: false
+        },
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    })
 }
 
 export const fishes = (req: Request, res: Response)=>{
-    res.send('fishes')
+    res.render('pages/page.mustache', {
+        menu: {
+            all: false,
+            dogs: false,
+            cats: false,
+            fishes: true
+        },
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    })
 }
